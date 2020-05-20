cd6b0a83553478eda1bebd7167794858f67757f4
Shunzhe Ma <me@msz.cool>
2020-05-20 11:57:54 -0400

    Initial Commit

---
+++ CoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML
+//
+//  Created by Shunzhe Ma on 5/20/20.
+//  Copyright © 2020 Shunzhe Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
